00194616be952857ea0168346dd376949167998c
htz-html <1006676722@qq.com>
2021-03-09 23:50:33 +0800

    row 和 col 基本写出来了

---
+++ src/col.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="col" :class="[`col-${span}`]">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name:"DoveCol",
+  props:{
+    span: {
+      type:[Number, String]
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .col{
+    width: 50%;
+    height: 40px;
+    background: #564bf7;
+    border: 1px solid red;
+
+    $class-prefix:col-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+  }
+</style>
