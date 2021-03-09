c6a02f8b53bf261bcbc461e48e41c17074f43e34
htz-html <1006676722@qq.com>
2021-03-10 00:29:48 +0800

    基本实现山歌式布局

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
-    <slot></slot>
+  <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
+  :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+ 'px'}">
+    <div style="border:1px solid red;height: 40px;">
+      <slot></slot>
+    </div>
   </div>
 </template>
 <script>
@@ -9,6 +12,14 @@ export default {
   props:{
     span: {
       type:[Number, String]
+    },
+    offset:{
+      type: [Number, String]
+    }
+  },
+  data(){
+    return {
+      gutter: 0
     }
   }
 }
@@ -16,14 +27,18 @@ export default {
 <style lang="scss" scoped>
   .col{
     width: 50%;
-    height: 40px;
-    background: #564bf7;
-    border: 1px solid red;
+    
 
     $class-prefix:col-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
         width: ($n / 24) * 100%;
+      }
+    }
+    $class-prefix:offset-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
       }
     }
   }
