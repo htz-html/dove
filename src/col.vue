fc8bab4e322740ad7bdb087721410855bd07ffd4
htz-html <1006676722@qq.com>
2021-03-10 16:06:49 +0800

    响应式修改成移动端优先 的模式

--- src/col.vue
+++ src/col.vue
@@ -82,7 +82,7 @@ export default {
         margin-left: ($n / 24) * 100%;
       }
     }
-    @media (min-width: 577px) and (max-width: 768px)  {
+    @media (min-width: 577px) {
       $class-prefix:col-ipad-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
@@ -96,7 +96,7 @@ export default {
         }
       }
     }
-    @media (min-width: 769px) and (max-width: 992px)  {
+    @media (min-width: 769px){
       $class-prefix:col-narrow-pc-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
@@ -110,7 +110,7 @@ export default {
         }
       }
     }
-    @media (min-width: 993px) and (max-width: 1200px)  {
+    @media (min-width: 993px) {
       $class-prefix:col-pc-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
