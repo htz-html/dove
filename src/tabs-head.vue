0f86a474e294f4688b94d2d0201ba704b8409f34
htz-html <1006676722@qq.com>
2021-03-11 16:33:40 +0800

    基本完成tabs

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -23,12 +23,14 @@ export default {
 <style lang="scss" scoped>
   $blue: rgb(45, 30, 255);
   $tab-height:40px;
+  $border-color: #ddd;
   .tabs-head{
     display: flex;
     height: $tab-height;
     justify-content: flex-start;
     align-items: center;
     position: relative;
+    border-bottom: 1px solid $border-color;
     > .line{
       position: absolute;
       bottom: 0;
@@ -37,6 +39,10 @@ export default {
     }
     > .actions-wrapper{
       margin-left: auto;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding: 0 1em;
     }
   }
 </style>
