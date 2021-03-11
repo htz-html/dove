e97255ad5703155d0c09996ae12ba59f0f8d1da4
htz-html <1006676722@qq.com>
2021-03-11 16:41:39 +0800

    支持 disabled 功能

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tabs-item" @click="xxx" :class="classes">
+  <div class="tabs-item" @click="onClick" :class="classes" :disabled="disabled">
     <slot></slot>
   </div>
 </template>
@@ -25,7 +25,8 @@ export default {
   computed:{
     classes(){
       return {
-        active: this.active
+        active: this.active,
+        disabled: this.disabled
       }
     }
   },
@@ -35,7 +36,8 @@ export default {
     })
   },
   methods:{
-    xxx(){
+    onClick(){
+      if(this.disabled){ return }
       this.eventBus.$emit('update:selected', this.name, this)
     }
   }
@@ -43,6 +45,7 @@ export default {
 </script>
 <style lang="scss" scoped>
   $blue: rgb(45, 30, 255);
+  $disabled-text-color: #aaa;
   .tabs-item{
     flex-shrink: 0;
     padding: 0 1em;
@@ -50,10 +53,14 @@ export default {
     height: 100%;
     display: flex;
     align-items: center;
-    color: #888;
+    color: #333;
     &.active{
       color: $blue;
       font-weight: bold;
+    }
+    &.disabled{
+      cursor: not-allowed;
+      color: $disabled-text-color;
     }
   }
 </style>
