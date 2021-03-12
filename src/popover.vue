39eaf20314bed8c8a0513b8770c0d8cc9eb9d119
htz-html <1006676722@qq.com>
2021-03-12 20:02:29 +0800

    popover 的四个方向的搞定

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="popover" @click="onClick" ref="popover">
-    <div ref="contentWrapper" v-if="visible" class="content-wrapper">
+    <div ref="contentWrapper" v-if="visible" 
+      class="content-wrapper"
+      :class="{[`position-${position}`]:true}"
+      >
       <slot name="content"></slot>
     </div>
     <span ref="triggerWrapper" style="display: inline-block">
@@ -16,12 +19,37 @@ export default {
       visible: false
     }
   },
+  props:{
+    position:{
+      type: String,
+      default: 'top',
+      validator(value){
+        return ['top', 'left', 'right', 'bottom'].includes(value) >=0
+      }
+    }
+  },
   methods:{
     positionContent(){
+      const {contentWrapper,triggerWrapper} = this.$refs
       document.body.appendChild(this.$refs.contentWrapper)
-      let {width,height,left,top}=this.$refs.triggerWrapper.getBoundingClientRect()
-      this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
-      this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+      let {width,height,left,top}=triggerWrapper.getBoundingClientRect()
+      if(this.position === 'top'){
+        contentWrapper.style.left = left + window.scrollX + 'px'
+        contentWrapper.style.top = top + window.scrollY + 'px'
+      } else if(this.position === 'bottom'){
+        contentWrapper.style.left = left + window.scrollX + 'px'
+        contentWrapper.style.top = top + height + window.scrollY + 'px'
+      } else if(this.position === 'left'){
+        contentWrapper.style.left = left + window.scrollX + 'px'
+        let {height: height2} = contentWrapper.getBoundingClientRect()
+        contentWrapper.style.top = top + window.scrollY +
+        (height-height2)/2 + 'px'
+      }else if(this.position === 'right'){
+        contentWrapper.style.left = left + window.scrollX + width + 'px'
+        let {height: height2} = contentWrapper.getBoundingClientRect()
+        contentWrapper.style.top = top + window.scrollY +
+        (height-height2)/2 + 'px'
+      }
     },
     onClickDocument(e){
       if(this.$refs.popover &&  
@@ -71,9 +99,7 @@ export default {
     border-radius: $border-raidus;
     filter: drop-shadow(0 1px 1px rgba(0,0,0,0.5));
     background: white;
-    transform: translateY(-100%);
     padding: 0.5em 1em;
-    margin-top: -10px;
     max-width: 20em;
     word-break: break-all;
     &::after,&::before{
@@ -83,15 +109,66 @@ export default {
       width: 0;
       height: 0;
       position: absolute;
-      left: 10px;
     }
-    &::before{
-      border-top-color: black;
-      top: 100%;
+    &.position-top{
+      transform: translateY(-100%);
+      margin-top: -10px;
+      &::after,&::before{
+        left: 10px;
+      }
+      &::before{
+        border-top-color: black;
+        top: 100%;
+      }
+      &::after{
+        border-top-color: white;
+        top: calc(100% - 1px);
+      }
     }
-    &::after{
-      border-top-color: white;
-      top: calc(100% - 1px);
+    &.position-bottom{
+      margin-top: 10px;
+      &::after,&::before{
+        left: 10px;
+      }
+      &::before{
+        border-bottom-color: black;
+        bottom: 100%;
+      }
+      &::after{
+        border-bottom-color: white;
+        bottom: calc(100% - 1px);
+      }
+    }
+    &.position-left{
+      transform: translateX(-100%);
+      margin-left: -10px;
+      &::after,&::before{
+        transform: translateY(-50%);
+        top: 50%;
+      }
+      &::before{
+        border-left-color: black;
+        left: 100%;
+      }
+      &::after{
+        border-left-color: white;
+        left: calc(100% - 1px);
+      }
+    }
+    &.position-right{
+      margin-left: 10px;
+      &::after,&::before{
+        transform: translateY(-50%);
+        top: 50%;
+      }
+      &::before{
+        border-right-color: black;
+        right: 100%;
+      }
+      &::after{
+        border-right-color: white;
+        right: calc(100% - 1px);
+      }
     }
   }
 </style>
