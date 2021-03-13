8089f53195d286d8361d578b45c03b92d74257a5
htz-html <1006676722@qq.com>
2021-03-13 13:52:30 +0800

    collapse 基本样式和基本功能完成

---
+++ src/collapse-item.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="collapse-item">
+    <div class="title" @click="open=!open">
+      {{title}}
+    </div>
+    <div class="content" v-if="open">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "DoveCollapseItem",
+  data(){
+    return{
+      open: false
+    }
+  },
+  props:{
+    title:{
+      type:String,
+      required: true
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+$grey: #ddd;
+$border-radius: 4px;
+  .collapse-item{
+    > .title{
+      border: 1px solid $grey;
+      margin-left: -1px;
+      margin-right: -1px;
+      margin-top: -1px;
+      height: 40px;
+      display: flex;
+      align-items: center;
+      padding-left: 10px;
+    }
+    &:first-child{
+      >.title{
+        border-top-left-radius: $border-radius;
+        border-top-right-radius: $border-radius;
+      }
+    }
+    &:last-child{
+      >.title:last-child{
+        border-bottom-left-radius: $border-radius;
+        border-bottom-right-radius: $border-radius;
+      }
+    }
+    > .content{
+      padding: 10px;
+    }
+  }
+</style>
---
+++ src/collapse.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="collapse">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: "DoveCollapse"
+}
+</script>
+<style lang="scss" scoped>
+$grey: #ddd;
+  .collapse{
+    border: 1px solid $grey;
+    border-radius: 4px;
+  }
+</style>
