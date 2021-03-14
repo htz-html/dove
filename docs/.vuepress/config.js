6f0b34c496a7fcc25b25a5dee22df3e230f654e1
htz-html <1006676722@qq.com>
2021-03-14 14:57:41 +0800

    创建官网

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: '德芙 UI ',
+  description: '一个好用的 UI 框架',
+  themeConfig: {
+    sidebar: [
+      {
+        title: "入门",
+        children: ['/install/','/get-started/']
+      },
+      {
+        title: "组件",
+        children: ['/components/button']
+      },
+    ]
+  }
+}
--- src/button.vue
+++ src/button.vue
@@ -5,7 +5,7 @@
   >
     <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
     <g-icon v-if="loading" class="icon loading" name="loading"></g-icon>
-    <div class="content">
+    <div class="button-content">
       <slot></slot>
     </div>
   </button>
@@ -27,7 +27,6 @@ export default {
       type: String,
       default: 'left',
       validator(value){ //属性检查器，检查传过来的值是否正确
-        // return value !== 'left' && value !== 'right' ? false : true
         return !(value !== 'left' && value !== 'right');
       }
     }
@@ -35,30 +34,39 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+$button-height: 40px;
+$font-size: 14px;
+$button-bg: white;
+$button-active-bg: #eee;
+$border-radius: 4px;
+$color: #333;
+$border-color: #999;
+$border-color-hover: #666;
 @keyframes spin {
   0%{transform: rotate(0deg);}
   100%{transform: rotate(360deg);}
 }
 .g-button {
-  font-size: var(--font-size);
-  height: var(--button-height);
+  font-size: $font-size;
+  height: $button-height;
   padding: 0 1em;
-  border-radius: var(--border-radius);
-  border: 1px solid var(--border-color);
-  background: var(--button-bg);
-  display: inline-flex; justify-content: center; 
+  border-radius: $border-radius;
+  border: 1px solid $border-color;
+  background: $button-bg;
+  display: inline-flex; 
+  justify-content: center; 
   align-items: center;
   vertical-align: middle;
   &:hover {
-    border-color: var(--border-color-hover);
+    border-color: $border-color-hover;
   }
   &:active {
-    background-color: var(--button-active-bg);
+    background-color: $button-active-bg;
   }
   &:focus {
     outline: none;
   }
-  > .content{
+  > .button-content{
     order: 2;
   }
   > .icon{
@@ -70,7 +78,7 @@ export default {
       order: 2;
       margin-left: .3em;
     }
-    > .content{
+    > .button-content{
       order: 1;
     }
   }
